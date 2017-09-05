cb338e4cee75caeb9de6f34e73c07608ccfd4e53
Nikola Mirkovski <nikola.mirkovski@webfactory.mk>
2017-09-05 15:08:22 +0200

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello
-    render html: "Hello Sample_App"
-  end
+
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
   end
-
   def help
   end
   def about
   end
+  def contact
+  end
+
 end
+
+
+ #vo slucajov kaj static-pages samo prazni metodi koi sto povikuvaat view - zatoa e static nema nisto dinamicko
