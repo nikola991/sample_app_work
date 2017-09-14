61c95a19c04dc285c509d09baaabf4332b442dcd
Nikola Mirkovski <nikola.mirkovski@webfactory.mk>
2017-09-14 15:21:32 +0200

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  include SessionsHelper  #ovde e include za da go povikuvame vo site controleri
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,6 +13,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
 
   end
+
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
@@ -23,5 +24,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
   end
 end
