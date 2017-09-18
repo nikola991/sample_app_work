ea612b8b95cf0db9d5368a21e3276881ef2284a9
Nikola Mirkovski <nikola.mirkovski@webfactory.mk>
2017-09-18 17:22:00 +0200

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]    #za da moze da gi koristi tie funkcionalnosti prvo mora da e logiran, vtoro mora da e correct user
+  before_action :admin_user,     only: :destroy
   def new
     @user = User.new
   end
   def show
     @user = User.find(params[:id])
   end
+  def edit
+    #od before action ima instance @user zemeno
+  end
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+
 
   def create
     @user = User.new(user_params)
@@ -16,10 +32,35 @@ class UsersController < ApplicationController
     end
   end
 
-  private
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  def update
+    # od before_action ima @user
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user) #dokolku @user != od current user od session helper redirect
+  end
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
 
