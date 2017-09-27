bb92943a5cdb90a495ec2159afd6116ca33e7f3e
Nikola Mirkovski <nikola.mirkovski@webfactory.mk>
2017-09-27 13:48:39 +0200

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  has_many :active_relationships, class_name:  "Relationship",
+           foreign_key: "follower_id",
+           dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+           foreign_key: "followed_id",
+           dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
 
@@ -18,9 +27,11 @@ class User < ApplicationRecord
                BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
-
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
   # Returns a random token.
@@ -70,6 +81,20 @@ class User < ApplicationRecord
   # Returns true if a password reset has expired.
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+  # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
   private
 
